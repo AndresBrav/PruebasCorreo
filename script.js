9987d94683196562fea8463e6b6ad66acb57ee4b
Andres Bravo <andbrav1917@gmail.com>
2026-02-13 06:24:54 -0400

    javascript

---
+++ script.js
@@ -0,0 +1,11 @@
+// const boton = document.getElementById("mi-boton");
+
+// boton.addEventListener("click", () => {
+//     alert("Hola has apretado el boton");
+// });
+
+const boton2 = document.querySelector(".boton-design");
+
+boton2.addEventListener("click", () => {
+    alert("Hola has apretado el boton design");
+});
