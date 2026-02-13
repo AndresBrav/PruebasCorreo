7047bfe5b8a4fbcab19a22d5c63239b44cd34f3a
Andres Bravo <andbrav1917@gmail.com>
2026-02-13 10:09:59 -0400

    botones * query selector

--- script.js
+++ script.js
@@ -4,8 +4,24 @@
 //     alert("Hola has apretado el boton");
 // });
 
-const boton2 = document.querySelector(".boton-design");
+// const boton2 = document.querySelector(".boton-design");
 
-boton2.addEventListener("click", () => {
-    alert("Hola has apretado el boton design");
+// boton2.addEventListener("click", () => {
+//     alert("Hola has apretado el boton design");
+// });
+
+const boton = document.querySelector(".boton-design");
+const mensaje = document.getElementById("mensaje");
+
+let contador = 0;
+
+boton.addEventListener("click", () => {
+    if (contador % 2 === 0) {
+        mensaje.textContent = "¡Has hecho clic en el botón!";
+    } else {
+        mensaje.textContent = "Texto original";
+    }
+    contador++;
+    console.log(contador)
 });
+
